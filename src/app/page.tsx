00d5c308ff586386c02466c90a9dd5cec7be0260
ISAWASHUN <syun20000616@icloud.com>
2023-10-03 17:24:35 +0900

    shadcn/uiを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='text-lg'>
       メインページ
+      <Button>Click me</Button>
     </main>
   )
 }
