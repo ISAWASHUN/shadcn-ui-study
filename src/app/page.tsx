99597d44eec4071ede4c5c9f61e97a820a6f27eb
ISAWASHUN <syun20000616@icloud.com>
2023-10-03 17:59:18 +0900

    ページを作成・更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton
+} from "@clerk/nextjs";
 export default function Home() {
   return (
-    <main className='text-lg'>
-      メインページ
-      <Button>Click me</Button>
+    <main className="text-lg">
+      <Button>ボタン</Button>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>{" "}
     </main>
-  )
+  );
 }
